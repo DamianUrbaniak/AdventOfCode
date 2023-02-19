924bb447af138b8193f26d45be1e41d876586630
DamianUrbaniak <damianourbaniak@gmail.com>
2023-02-19 10:15:25 +0100

    Add fourth day solutions.

--- src/main/java/org/damianurbaniak/day4/SolutionForDay4.java
+++ src/main/java/org/damianurbaniak/day4/SolutionForDay4.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class SolutionForDay4 {
     public static void main(String[] args) throws FileNotFoundException {
         final List<int[][]> inputDay4_tables =
-                ConvertInputDay4.tablesForDay4("src/main/java/org/damianurbaniak/day4/inputDay4_numbers");
+                ConvertInputDay4.tablesForDay4("src/main/java/org/damianurbaniak/day4/inputDay4_tables");
         final List<Integer> inputDay4_numbers =
-                ConvertInputDay4.numbersForDay4("src/main/java/org/damianurbaniak/day4/inputDay4_tables");
+                ConvertInputDay4.numbersForDay4("src/main/java/org/damianurbaniak/day4/inputDay4_numbers");
 
         System.out.println(Message.FIRST_PART_SOLUTION.getMessage());
         System.out.println(SolvingMethodsDay4.finalResult(inputDay4_tables, inputDay4_numbers).part1());
